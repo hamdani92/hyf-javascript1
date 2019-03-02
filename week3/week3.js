6779acf316109caa50b3a1ab6e0a3f5c3c78d680
Syed Ibrahim Ahmed <34831534+hamdani92@users.noreply.github.com>
2019-03-02 21:24:51 +0100

    Update week3.js

--- week3/week3.js
+++ week3/week3.js
@@ -1,88 +1,105 @@
 
-//String and array challenge
-//console.log for this statement
 let myString = "hello,this,is,a,difficult,to,read,sentence";
 console.log(myString);
-//how to find out length of the strings or statement
 console.log(myString.length);
+let rep = myString.split(",").join(" ");
+console.log(rep);
 
-
-//how to remove a letter and replace 
-// /which letter to remove/g,' add the letter here which do you want to replace'
-myString=myString.replace(/,/g,' ');
-console.log(myString);
-
-//to push some data in array using push function 
 let favoriteAnimals = ["blowfish", "capricorn", "giraffe"];
-favoriteAnimals.push("turtle");
-
-//add a text in specific place
-//splice(index number,remove how much text do you want to remove,"text do you want to add")
+favoriteAnimals.push('turtle');
 console.log(favoriteAnimals);
-favoriteAnimals.splice(1,0,"meerkat");
+favoriteAnimals.splice(1, 0, 'meerka');
 console.log(favoriteAnimals);
-
-//to find out the lenght of array 
-console.log("The array has a length of" ,favoriteAnimals.length);
-favoriteAnimals.splice(3,1);
+console.log("the length of array is : ", favoriteAnimals.length);
+favoriteAnimals.splice('3', '1');
 console.log(favoriteAnimals);
 
-//to finout a specifice string at index number
-console.log('The item you are looking for is at index: ', favoriteAnimals.indexOf("meerkat"));
 favoriteAnimals.splice(favoriteAnimals.indexOf("meerkat"), 1);
 console.log(favoriteAnimals);
 
+function sum(a, b, c) {
+  return a + b + c;
+}
+console.log(sum(2, 6, 5));
+
+function colorcar(color) {
+  console.log("a" + color + "car");
+}
+colorcar("red");
 
 
 
-//more javascript
-function sum(a,b,c)
-
-{
-    
-    let result= a+b+c;
-return result;}
-let a=1;
-    let b=2;
-    let c=3;
-
-console.log("sum is :",a+b+c);
 
 
-function colorCar(red){
-        return "a " + red + " car";
-    }
+function vehicleType(color, code) {
+  if (code === 1) {
+    console.log("a", color, "car")
+  } else {
+    console.log("a", color, "motorbike")
+  };
 
-
-
-    let car = {
-        name: "nissan",
-        model:"GTR 2018",
-        horsePower: "600",
-        torque: "425",
-    };
-    for (let entries of Object.entries(car)){
-        console.log(entries);
-    };
-    function vehicleType(color,code)
-{
-
-    if (code===1)
-    {
-        return console.log("a",colored,"car")
-
-    }
-    else
-    {
-    return console.log("a",color ,"motorBike")
-    };
 };
+vehicleType("blue", 1);
 
-vehicleType("blue", 2);
-console.log("yes");
+console.log(3 === 3 ? "yes" : "no");
+//Create a function called vehicle, like before, but takes another parameter 
+//called age, so that vehicle("blue", 1, 5) prints 'a blue used car'
+function vehicle(color, code, age) {
+  if (code === 1 && age === 5) {
+    console.log("a", color, "used car");
+  } else if (code === 2 && age === 0) {
+    console.log("a", color, "motorbike")
 
-if (3 === 3) {
-     console.log("yes");
-    } else {
-     console.log("no");
-    }
+  }
+}
+vehicle("green", 1, 5);
+//Make a list of vehicles, you can add "motorbike", "caravan", "bike", or more.
+let vehicles = [
+  "motorbike", "caravan", "bike"
+]
+//How do you get the third element from that list?
+console.log(vehicles[2]);
+//Change the function vehicle to use the list of question 
+//7. So that vehicle("green", 3, 1) prints "a green new bike".
+function vehicle(color, code, age) {
+  if (code === 3 && age === 1) {
+    console.log("a", color, "new bike");
+  } else if (code === 2 && age === 0) {
+    console.log("a", color, "motorbike")
+
+  }
+}
+vehicle("green", 3, 1);
+
+//Use the list of vehicles to write an advertisement.So that it prints something 
+//like: "Amazing Joe's Garage, we service cars, motorbikes, caravans and bikes.".
+//(Hint: use a for loop.)
+
+let advertisement = [
+  "motorbike", "caravan", "bike"
+]
+for (let i = 0; i < 1; i++) {
+  console.log("amazing joe's Garage, we service cars,",
+    advertisement[0], ',', advertisement[1], "and", advertisement[2], ".");
+}
+
+let humanBody = {
+
+}
+let teachers = {
+  name: "sanaulla",
+  age: 40,
+  module: "C++"
+
+}
+let x = [1, 2, 3];
+let y = [1, 2, 3];
+let z = y;
+if (z == x) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
+let bar = 42;
+typeof typeof bar;
+console.log(bar)
